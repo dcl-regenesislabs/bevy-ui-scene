aa0fd9515911b756a6919fb61805d56989632865
pabloes <estornut@gmail.com>
2026-03-06 15:23:08 +0100

    refactor badges-preview from overview in other file

--- scene/src/ui-classes/main-hud/passport/passport-popup.tsx
+++ scene/src/ui-classes/main-hud/passport/passport-popup.tsx
@@ -81,6 +81,7 @@ import { PassportSection } from './passport-section'
 import { BadgesCollection } from './badges-collection'
 import { PASSPORT_SECTIONS } from './passport-constants'
 import { Badge3dPreviewElement } from './badge-3d-preview'
+import { BadgesPreview } from './badges-preview'
 
 export type PassportPopupState = {
   loadingProfile: boolean
@@ -344,77 +345,6 @@ function PassportContent(): ReactElement {
         ) : null}
       </Column>
     </UiEntity>
-  )
-}
-
-function BadgesPreview({
-  badgesData,
-  loading
-}: {
-  badgesData: AchievementsData | null
-  loading?: boolean
-}): ReactElement | null {
-  const fontSize = getFontSize({ context: CONTEXT.DIALOG })
-  const size = fontSize * 4
-  const loadingBadges = loading ?? false
-  const loadingAlpha = loadingBadges ? getLoadingAlphaValue() : 0
-
-  const previewBadges = badgesData?.achieved
-    .sort((a, b) => b.completedAt?.localeCompare(a.completedAt ?? '') ?? 0)
-    .slice(0, 9)
-
-  if (!loadingBadges && !previewBadges?.length) return null
-
-  return (
-    <PassportSection uiTransform={{ opacity: state.savingProfile ? 0.5 : 1 }}>
-      <UiEntity
-        uiText={{
-          value: '<b>BADGES</b>',
-          fontSize
-        }}
-      />
-      <Row
-        uiTransform={{
-          alignItems: 'space-around',
-          justifyContent: 'space-around'
-        }}
-      >
-        {loadingBadges ? (
-          <UiEntity
-            uiTransform={{
-              width: '98%',
-              height: size,
-              alignSelf: 'flex-start',
-              borderRadius: fontSize / 2,
-              borderWidth: 0,
-              borderColor: COLOR.BLACK_TRANSPARENT
-            }}
-            uiBackground={{
-              color: Color4.create(1, 1, 1, loadingAlpha * 0.3)
-            }}
-            uiText={{
-              value: '...',
-              fontSize,
-              color: COLOR.WHITE_OPACITY_5
-            }}
-          />
-        ) : null}
-        {previewBadges?.map((previewBadge) => {
-          return (
-            <UiEntity
-              uiTransform={{
-                width: size,
-                height: size
-              }}
-              uiBackground={{
-                texture: { src: `${previewBadge.assets['2d']?.normal}` },
-                textureMode: 'stretch'
-              }}
-            />
-          )
-        })}
-      </Row>
-    </PassportSection>
   )
 }
 
